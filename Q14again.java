74e89a78a3a6383bfaedae52d56618485b0d1894
Etelson Alcius <69489350+Sonnyywithanyextray@users.noreply.github.com>
2023-07-11 15:22:24 -0400

    Successful implementation of temperature conversion process.
    Implementation of conversion process:  Worked on lines 49-70. Essentially, depending on which symbol is more present in the file, and depending on whether it equals "C" or "F" the program will take each number within the nums array, perform the appropriate calculation on it, and shove it into the nums2[i] array, and if it doesn't equal the appropriate character, it'll set the value at i in nums2[i] equal to numbers[i], the original value in the nums array, and leave it the way it is. It'll then print each value within the nums2 array,  and the appropriate symbol that corresponds with it.

--- Q14again.java
+++ Q14again.java
@@ -1,36 +1,34 @@
 public class Q14again {
-    public static void main (String [] args) {
-        In opener = new In ("temperature.txt");
-        Out closer = new Out ("normalized-temperature.txt");
+    public static void main(String[] args) {
+        In opener = new In("temperature.txt");
+        Out closer = new Out("normalized-temperature.txt");
 
-        String [] nums = null;
-        String [] chars = null;
-        double [] nums2 = new double [nums.length];
-     
+        float[] nums = null;
+        String[] chars = null;
 
         while (!opener.isEmpty()) {
             String line = opener.readLine();
-            String [] parts = line.split(" ");
+            String[] parts = line.split(" ");
 
-            int numcount=0;
+            int numcount = 0;
 
-            for (int i = 0; i<parts.length; i++) {
-                if (parts[i].equalsIgnoreCase("C")||parts[i].equalsIgnoreCase("F"))
-                numcount++;
+            for (int i = 0; i < parts.length; i++) {
+                if (parts[i].equalsIgnoreCase("C") || parts[i].equalsIgnoreCase("F"))
+                    numcount++;
             }
 
-            nums = new String [numcount];
+            nums = new float[numcount];
             chars = new String[numcount];
 
-            int a =0;
-            int b =0;
-            for (int i =0; i<parts.length; i++) {
-                if (parts[i].equalsIgnoreCase("C")||parts[i].equalsIgnoreCase("F")) {
+            int a = 0;
+            int b = 0;
+
+            for (int i = 0; i < parts.length; i++) {
+                if (parts[i].equalsIgnoreCase("C") || parts[i].equalsIgnoreCase("F")) {
                     chars[a] = parts[i];
                     a++;
-                }
-                else {
-                    nums[b] = parts[i];
+                } else {
+                    nums[b] = Float.parseFloat(parts[i]);
                     b++;
                 }
             }
@@ -40,7 +38,7 @@ public class Q14again {
             int Ccounter = 0;
             int Fcounter = 0;
 
-            for (int i =0; i<parts.length; i++) {
+            for (int i = 0; i < parts.length; i++) {
                 if (parts[i].equals("C")) {
                     Ccounter++;
                 }
@@ -48,23 +46,37 @@ public class Q14again {
                     Fcounter++;
                 }
             }
-            if (Ccounter>Fcounter) {
-                b=0;
-                for (int i = 0; i<nums2.length; i++) {
-                    if (parts[i].equals("F")) {
-                        double newval = Double.parseDouble(nums[b]);
-                        nums2[i] = newval;
-                        double newval2 = (newval-32) *0.55555556;
-                        double c = newval2;
+            double[] nums2 = new double[nums.length];
 
-                        closer.print(c + " " + "C ");
-                        b++;
+            if (Ccounter > Fcounter) {
+                System.out.println("Celsius");
+                for (int i = 0; i < nums.length; i++) {
+                    if (chars[i].equals("F")) {
+                        nums2[i] = (int) ((nums[i] - 32) * 5 / 9);
+                    } else {
+                        nums2[i] = (int) nums[i];
                     }
-                    }
+                    closer.printf(nums2[i] + " " + "C " + " ");
+
                 }
+            } else {
+                System.out.println("Farenheit");
+                for (int i = 0; i < nums.length; i++) {
+                    if (chars[i].equals("C")) {
+                        nums2[i] = (int) (nums[i] * 9 / 5 + 32);
+                    } else {
+                        nums2[i] = (int) nums[i];
+                    }
+                    closer.printf(nums2[i] + " " + "F " + " ");
+                }
+            }
+
+            
             }
         }
     }
+
+
 
 
 
